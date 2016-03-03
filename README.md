6831b9cb88b0809a31e0cbd81c44abe4d71042bd
Colin Bate <colin@colinbate.com>
2016-03-02 22:38:03 -0400

    Initial plugin

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # replace-brunch
-Perform generic replacement on your compiled files or assets
+
+Perform generic replacements on your compiled files or assets.
+
+## Config Options
+
+If you add a `replace` property to the plugins object in your brunch config you can configure this plugin.
+
+There are two config options.
+
+### `environment`
+
+This simply states which environments to run the replacements in. By default it does all of them. But if you set this to `"production"` then it will only run the replacements when doing a production build.
+
+### `replacements`
+
+This is an array filled with objects which look this this:
+
+```js
+{
+  files: /\.js$/,
+  matches: [
+    {
+      find: 'SOME_TOKEN',
+      replace: 'My Name'
+    }
+  ]
+}
+```
+
+The `files` property is an `anymatch` matcher for the files in your public output directory.
+
+The `matches` property is an array of match objects, each with a find and replace property. The `find` value can be a string or a RegExp. Replace can be a function just like accepted by `string.replace` only with the first parameter set to the contents of the package.config.
---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+const path = require('path');
+const anymatch = require('anymatch');
+const fs = require('fs');
+
+class ReplacePlugin {
+  constructor(config) {
+    this.config = config && config.plugins && config.plugins.replace || {};
+    this.package = {};
+    if (this.config.environment) {
+      this.defaultEnv = this.config.environment;
+    }
+    if (config.paths.packageConfig) {
+      const pconf = require(path.resolve(config.paths.packageConfig));
+      if (pconf) {
+        this.package = pconf;
+      }
+    }
+    this.replacements = this.config.replacements || [{
+      files: [/\.js$/, /\.html$/],
+      match: {find: '@@VERSION@@', replace: p => p.version}
+    }];
+    
+    // Normalize
+    this.replacements.forEach(rep => {
+      if (rep.files) {
+        rep.files = anymatch(rep.files);
+      } else {
+        rep.files = () => true;
+      }
+      if (!rep.matches) {
+        rep.matches = [];
+      }
+      if (rep.match) {
+        rep.matches.push(rep.match);
+        delete rep.match;
+      }
+      rep.matches.forEach(m => {
+        if (typeof m.find === 'string') {
+          m.find = new RegExp(m.find, 'g');
+        }
+        if (typeof m.replace === 'function') {
+          let fn = m.replace;
+          m.replace = () => {
+            var args = Array.prototype.slice.call(arguments);
+            return fn(this.package, ...args);
+          };
+        }
+      });
+    });
+  }
+
+  onCompile(files, assets) {
+    let counter = 0;
+    const all = files.map(x => x.path).concat(assets.map(x => x.destinationPath))
+    all.forEach(file => {
+      let matchers = this.replacements.reduce((p,c) => c.files(file) ? p.concat(c.matches) : p, []);
+      if (!matchers.length) {
+        return;
+      }
+      let content = fs.readFileSync(file, 'utf8');
+      matchers.forEach(m => {
+        content = content.replace(m.find, m.replace);
+        counter++;
+      });
+      fs.writeFileSync(file, content, 'utf8');
+    });
+  }
+
+}
+
+ReplacePlugin.prototype.brunchPlugin = true;
+
+module.exports = ReplacePlugin;
